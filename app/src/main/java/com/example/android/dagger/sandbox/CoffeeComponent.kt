162b82764cef3151245ef8e78754cc2636c761df
yoosangyeob <syyoo@joara.com>
2022-09-05 21:24:39 +0900

    sandbox singleton inject!!

--- app/src/main/java/com/example/android/dagger/sandbox/CoffeeComponent.kt
+++ app/src/main/java/com/example/android/dagger/sandbox/CoffeeComponent.kt
@@ -1,7 +1,9 @@
 package com.example.android.dagger.sandbox
 
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [CoffeeMakerModule::class])
 interface CoffeeComponent {
     fun inject(activity: SandboxActivity)
--- app/src/main/java/com/example/android/dagger/sandbox/CoffeeMakerModule.kt
+++ app/src/main/java/com/example/android/dagger/sandbox/CoffeeMakerModule.kt
@@ -2,12 +2,18 @@ package com.example.android.dagger.sandbox
 
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class CoffeeMakerModule {
+
+    @Singleton
     @Provides
     fun provideHeater(): Heater = A_Heater()
 
     @Provides
-    fun provideCoffeeMaker(heater: Heater): CoffeeMaker = CoffeeMaker(heater, A_Pump(heater))
+    fun providePump(heater: Heater) : Pump = A_Pump(heater)
+
+    @Provides
+    fun provideCoffeeMaker(heater: Heater, pump: Pump): CoffeeMaker = CoffeeMaker(heater, pump)
 }
