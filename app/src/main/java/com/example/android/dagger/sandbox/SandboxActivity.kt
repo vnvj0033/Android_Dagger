90c657ebc0039dcac5f89c7fa3aac0b4b8fdc530
yoosangyeob <syyoo@joara.com>
2022-09-15 18:50:45 +0900

    snadbox di setting fragment inject setting

--- app/src/main/java/com/example/android/dagger/sandbox/SandboxActivity.kt
+++ app/src/main/java/com/example/android/dagger/sandbox/SandboxActivity.kt
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.android.dagger.R
 import com.example.android.dagger.sandbox.log.LogFragment
+import com.example.android.dagger.sandbox.setting.SettingFragment
 import com.example.android.dagger.sandbox.user.UserFragment
 
 class SandboxActivity: AppCompatActivity() {
 
     private val userFragment: UserFragment = UserFragment()
     private val logFragment: LogFragment = LogFragment()
+    private val settingFragment: SettingFragment = SettingFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val appComponent = DaggerAppComponent.factory().create(this)
@@ -18,13 +20,14 @@ class SandboxActivity: AppCompatActivity() {
         appComponent.userComponent().create(this).inject(userFragment)
         appComponent.logComponent().create(this).inject(logFragment)
 
-        appComponent.settingComponent().create().inject(this)
+        appComponent.settingComponent().create().inject(settingFragment)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_enter_details)
 
         userFragment.show()
         logFragment.show()
+        settingFragment.show()
     }
 
     private fun Fragment.show() {
--- app/src/main/java/com/example/android/dagger/sandbox/setting/SettingModule.kt
+++ app/src/main/java/com/example/android/dagger/sandbox/setting/SettingModule.kt
@@ -7,12 +7,6 @@ import javax.inject.Qualifier
 @Module
 class SettingModule {
 
-    @SettingScope
     @Provides
     fun setting(factory: SettingComponent.Factory) = Setting(factory)
 }
-
-
-@Retention(AnnotationRetention.BINARY)
-@Qualifier
-annotation class SettingScope
